61aa0963636cf227b21861fcf9a3c27af87e8732
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-04-16 12:52:16 +0530

    [Yukta]: added seat-service initial project structure

---
+++ seat-service/src/test/java/com/skybooker/seat/SeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skybooker.seat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
